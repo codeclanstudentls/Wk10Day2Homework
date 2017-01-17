66cff67c8cff00f9327574062f5d7e319ffe9a2b
Logan Smith <hamspamandeggs@gmail.com>
2017-01-17 22:32:33 +0000

    Hmm now thinking I dont need to have a customer and this could be done with another function in basket maybe

---
+++ specs/customer_spec.js
@@ -0,0 +1,12 @@
+var basket = require('../shopping_basket');
+var item = require('../item');
+var assert = require('assert');
+
+describe('Customer', function){
+
+  it('should see if customer has discount card', function())
+
+
+
+
+});
