753c79e069ef3a64313ef3fa2f6b7dcee4794d75
Logan Smith <hamspamandeggs@gmail.com>
2017-01-18 07:36:25 +0000

    Think I am over complicating this 5 percent loyalty discount card situation

--- customer.js
+++ customer.js
@@ -1,5 +1,17 @@
 var customer = {
   name: "Mr. Shops-a-lot",
-
+  
 }
 
+loyalty_card: function(){
+  if(loyalty_card === "yes"){
+    total.amount = total.amount * 0.05;
+    return "Loyalty card holders get a discount";
+  }
+  else total.amount = total.amount;
+    return total.amount;
+  };
+}
+
+
+module.exports = customer;
